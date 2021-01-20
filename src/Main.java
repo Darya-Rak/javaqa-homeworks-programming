0a0ddbf0dd9ba6f5223775da1d423c6f154bffb3
Darya-Rak <Darya-Rak@users.noreply.github.com>
2021-01-20 15:25:33 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int balance=2_000_000_000;
-        int amount=500_000_000;
-        int total= balance+amount;
+        long balance=2_000_000_000;
+        long amount=500_000_000;
+        long total= balance+amount;
         System.out.println(total);
     }
 }
